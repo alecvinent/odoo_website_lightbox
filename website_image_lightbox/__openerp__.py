78f79fe5e2e97c38d94a51fb2c607bdd1edce281
root <root@empire.creamsroom.com>
2014-11-16 20:05:25 -0500

    [ADD] website_image_lightbox: Adds lightbox js plugin to odoo's frontend assets

---
+++ website_image_lightbox/__openerp__.py
@@ -0,0 +1,11 @@
+{
+    'name': 'Website Image Lightbox',
+    'category': 'Website',
+    'summary': 'Allow to use classes of lightbox',
+    'version': '1.0',
+    'description': """Allow to use classes of lightbox""",
+    'author': 'Omar Castiñeira Saavedra',
+    'depends': ['website'],
+    'installable': True,
+    'data': ['views/website_lightbox_view.xml'],
+}
